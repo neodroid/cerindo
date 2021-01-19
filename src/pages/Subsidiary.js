95402dca66ed77b8db27a18908fac109eb754c07
jonathanSendiko <jonathan.sendiko@gmail.com>
2021-01-20 00:27:03 +0700

    subsidiar fix

--- src/pages/Subsidiary.js
+++ src/pages/Subsidiary.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { aboutUsService } from "../service/Aboutus";
 import Banner from "../components/Banner";
-import {SubsidiaryHead} from "../components/Data/AboutusData/SubsidiaryDatas";
+import { SubsidiaryHead } from "../components/Data/AboutusData/SubsidiaryDatas";
 import SubsidiaryComponents from "../components/AboutUsComponents/SubsidiaryComponents";
 
 const Subsidiary = () => {
+  const [banner, setBanner] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await aboutUsService.getListAboutUs();
+      const data = response.data;
+      setBanner(data.aboutUsBanner);
+    };
+    fetchData();
+  }, []);
+  if (banner.length === 0) return null;
   return (
     <>
-      <Banner {...SubsidiaryHead}/>
+      <Banner
+        img={banner[1].url}
+        heading="COMPANY SUBSIDIARY"
+        lightText={true}
+      />
       <SubsidiaryComponents />
     </>
   );
