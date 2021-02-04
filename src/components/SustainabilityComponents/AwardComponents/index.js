dc150d169a9b70771cd08345e6c2c38bdca08bd9
jonathanSendiko <jonathan.sendiko@gmail.com>
2021-02-04 19:29:33 +0700

    fix

--- src/components/SustainabilityComponents/AwardComponents/index.js
+++ src/components/SustainabilityComponents/AwardComponents/index.js
@@ -3,20 +3,15 @@ import { sustainService } from "../../../service/Sustainability";
 import { BusinessWrited } from "../../BusinessComponents/BusinessElements";
 import { Aboutstyle, AboutMain } from "../../AboutUsComponents/AboutUsElements";
 import BusinessSideBarComponents from "../index";
-<<<<<<< HEAD
 import { AwardImageContent } from "../../Data/SustainabilityData/AwardDatas";
 import {
   AwardsesContent,
   ContentAward,
   ImageAward,
   Linked,
+  Containerrs,
 } from "./AwardElements";
 import { BoxContainer } from "../../CorporateNewsComponents/NewsReleaseComponents/NewsContentComponents/NewsContentElements";
-=======
-import {AwardImageContent} from "../../Data/SustainabilityData/AwardDatas"
-import { AwardsesContent, ContentAward, ImageAward,Linked,Containerrs } from "./AwardElements";
-import {BoxContainer} from "../../CorporateNewsComponents/NewsReleaseComponents/NewsContentComponents/NewsContentElements";
->>>>>>> 2184e2ebda8eeb450d4b14e56f1286fff6a44747
 import { langContext } from "../../../langContext";
 import { AwardContentLangTitle, AwardContentLangBody } from "./AwardLang";
 
@@ -52,16 +47,10 @@ const AwardComponents = () => {
                   <Linked href={data.file.url} target="_blank">
                     <BoxContainer titlee>{data.name}</BoxContainer>
                   </Linked>
-<<<<<<< HEAD
-                  <embed src={data.file.url} width="300px" height="400px" />
-                  {/* <ImageAward src={data.image}/> */}
-                  <BoxContainer>{data.description}</BoxContainer>
-=======
                   <Containerrs>
-                    <ImageAward src={data.image}/>
+                    <ImageAward src={data.image} />
                     <BoxContainer>{data.caption}</BoxContainer>
                   </Containerrs>
->>>>>>> 2184e2ebda8eeb450d4b14e56f1286fff6a44747
                 </ContentAward>
               );
             })}
