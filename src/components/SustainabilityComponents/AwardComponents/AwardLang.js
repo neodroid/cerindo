0e1bf0d13a6dc4aa380b0e66495d32039eb56b86
jonathanSendiko <jonathan.sendiko@gmail.com>
2021-02-10 20:19:34 +0700

    award

--- src/components/SustainabilityComponents/AwardComponents/AwardLang.js
+++ src/components/SustainabilityComponents/AwardComponents/AwardLang.js
@@ -1,25 +1,46 @@
-import React from 'react'
+import React from "react";
 
 export const AwardContentLangTitle = (AwardContent, lang) => {
-    var text = 
-    (lang === "en") ? 
-        <div>{AwardContent.title_en}</div> :
-    (lang === "id") ? 
-        <div>{AwardContent.title_id}</div> :
-    (lang === "cn") ? 
-        <div>{AwardContent.title_ch}</div> : <> </>;
+  var text =
+    lang === "en" ? (
+      <div>{AwardContent.title_en}</div>
+    ) : lang === "id" ? (
+      <div>{AwardContent.title_id}</div>
+    ) : lang === "cn" ? (
+      <div>{AwardContent.title_ch}</div>
+    ) : (
+      <> </>
+    );
 
-    return text
-}
+  return text;
+};
 
 export const AwardContentLangBody = (AwardContent, lang) => {
-    var text = 
-    (lang === "en") ? 
-        <div>{AwardContent.body_en}</div> :
-    (lang === "id") ? 
-        <div>{AwardContent.body_id}</div> :
-    (lang === "cn") ? 
-        <div>{AwardContent.body_ch}</div> : <> </>;
+  var text =
+    lang === "en" ? (
+      <div>{AwardContent.body_en}</div>
+    ) : lang === "id" ? (
+      <div>{AwardContent.body_id}</div>
+    ) : lang === "cn" ? (
+      <div>{AwardContent.body_ch}</div>
+    ) : (
+      <> </>
+    );
 
-    return text
-}
+  return text;
+};
+
+export const AwardContentLangDescription = (data, lang) => {
+  var text =
+    lang === "en" ? (
+      <div>{data.description_en}</div>
+    ) : lang === "id" ? (
+      <div>{data.description_en}</div>
+    ) : lang === "cn" ? (
+      <div>{data.description_en}</div>
+    ) : (
+      <> </>
+    );
+
+  return text;
+};
