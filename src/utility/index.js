1b7c3b170192a12ac67f8caefc99b1430e6a90da
jonathanSendiko <jonathan.sendiko@gmail.com>
2021-02-10 20:20:00 +0700

    util

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:1337";
+const BASE_URL = "http://13.250.45.21/api";
 
 async function http(method, endpoint, body = null, data) {
   const headers = {
