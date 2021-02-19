db5c33f2247a5d3eb0e9fc32ffc790aa153218e7
jonathanSendiko <jonathan.sendiko@gmail.com>
2021-02-19 17:27:06 +0700

    util

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:1337";
+const BASE_URL = "https://cerindocorp.com/api";
 
 async function http(method, endpoint, body = null, data) {
   const headers = {
