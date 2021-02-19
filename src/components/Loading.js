5e36e955771e884a2eb6a1cd5166a0f40e22ce75
thaa15 <74519289+thaa15@users.noreply.github.com>
2021-02-19 14:45:34 +0700

    fade

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
-export const Loadings = styled.div`
+export const Loadingses = styled.div`
     text-align: center;
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100%;
     height: 100vh;
+    z-index: 2000;
 `;
