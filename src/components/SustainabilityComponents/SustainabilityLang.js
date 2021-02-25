b07d905cd266ab5639ea707fc648984aa4cd0220
jonathanSendiko <jonathan.sendiko@gmail.com>
2021-02-25 17:03:14 +0700

    keberlanjutan

--- src/components/SustainabilityComponents/SustainabilityLang.js
+++ src/components/SustainabilityComponents/SustainabilityLang.js
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export const SustainabilityTitle = (lang) => {
-    var text = 
-    (lang === "en") ? 
-        <>Global Sustainability</> :
-    (lang === "id") ? 
-        <>Keberlangsungan Global</> :
-    (lang === "cn") ? 
-        <>全球可持续发展</> : <> </>;
+  var text =
+    lang === "en" ? (
+      <>Global Sustainability</>
+    ) : lang === "id" ? (
+      <>Keberlanjutan Global</>
+    ) : lang === "cn" ? (
+      <>全球可持续发展</>
+    ) : (
+      <> </>
+    );
 
-    return text
-}
+  return text;
+};
 
 export const SustainabilitySidebar = (data, lang, index) => {
-    var text = 
-    (lang === "en") ? 
-        <>{data[index].en}</> :
-    (lang === "id") ? 
-        <>{data[index].id}</> :
-    (lang === "cn") ? 
-        <>{data[index].ch}</> : <> </>;
+  var text =
+    lang === "en" ? (
+      <>{data[index].en}</>
+    ) : lang === "id" ? (
+      <>{data[index].id}</>
+    ) : lang === "cn" ? (
+      <>{data[index].ch}</>
+    ) : (
+      <> </>
+    );
 
-    return text
-}
+  return text;
+};
